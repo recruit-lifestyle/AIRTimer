980dcbed1aee2918155463e6baa876e464cf51c8
Yuki Nagai <ynagai@r.recruit.co.jp>
2015-10-01 12:11:39 +0900

    Initial commit

---
+++ AIRTimer/AIRTimer.h
@@ -0,0 +1,19 @@
+//
+//  AIRTimer.h
+//  AIRTimer
+//
+//  Created by Yuki Nagai on 9/25/15.
+//  Copyright © 2015 Yuki Nagai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AIRTimer.
+FOUNDATION_EXPORT double AIRTimerVersionNumber;
+
+//! Project version string for AIRTimer.
+FOUNDATION_EXPORT const unsigned char AIRTimerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AIRTimer/PublicHeader.h>
+
+
---
+++ AIRTimer/AIRTimer.swift
@@ -0,0 +1,79 @@
+//
+//  AIRTimer.swift
+//  AIRTimer
+//
+//  Created by Yuki Nagai on 9/25/15.
+//  Copyright © 2015 Yuki Nagai. All rights reserved.
+//
+
+import Foundation
+
+public typealias TimerHandler = (AIRTimer) -> Void
+
+private final class TimerActor {
+    let timer: AIRTimer
+    let handler: TimerHandler
+    
+    init(timer: AIRTimer, handler: TimerHandler) {
+        self.timer = timer
+        self.handler = handler
+    }
+    
+    @objc func fire() {
+        handler(timer)
+    }
+}
+
+/**
+Restartable NSTimer Wrapper.
+
+> [You should not attempt to subclass NSTimer.](https://developer.apple.com/library/mac/documentation/Cocoa/Reference/Foundation/Classes/NSTimer_Class/)
+*/
+public final class AIRTimer {
+    public let interval: NSTimeInterval
+    public let userInfo: AnyObject?
+    public let repeats: Bool
+    private let handler: TimerHandler
+    private lazy var timer: NSTimer = { [unowned self] in
+        let actor = TimerActor(timer: self, handler: self.handler)
+        return NSTimer.scheduledTimerWithTimeInterval(self.interval, target: actor, selector: "fire", userInfo: self.userInfo, repeats: self.repeats)
+    }()
+    
+    public class func after(interval: NSTimeInterval, userInfo: AnyObject? = nil, handler: TimerHandler) -> AIRTimer {
+        let air = AIRTimer(timerInterval: interval, userInfo: userInfo, repeats: false, handler: handler)
+        air.timer.fire()
+        return air
+    }
+
+    public class func every(interval: NSTimeInterval, userInfo: AnyObject? = nil, handler: TimerHandler) -> AIRTimer {
+        let air = AIRTimer(timerInterval: interval, userInfo: userInfo, repeats: true, handler: handler)
+        air.timer.fire()
+        return air
+    }
+    
+    /**
+    Restart timer with current interval.
+    
+    - return: Regenerated AIRTimer. Your property should reference this.
+    */
+    public func restart() -> AIRTimer {
+        invalidate()
+        let air = AIRTimer(timerInterval: interval, userInfo: userInfo, repeats: repeats, handler: handler)
+        air.timer.fire()
+        return air
+    }
+    
+    /**
+    Stops the receiver from ever firing again and requests its removal from its run loop.
+    */
+    public func invalidate() {
+        timer.invalidate()
+    }
+    
+    private init(timerInterval interval: NSTimeInterval, userInfo: AnyObject?, repeats: Bool, handler: TimerHandler) {
+        self.interval = interval
+        self.userInfo = userInfo
+        self.repeats = repeats
+        self.handler = handler
+    }
+}
